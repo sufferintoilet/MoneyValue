834697ed217d382c95c75c9d1ba5e92878db17a9
sufferintoilet <nguyenhuynhquocan1@gmail.com>
2023-12-07 17:30:01 +0900

    1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace MoneyValue
+{
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int VND;
+        int USD;
+
+        Console.WriteLine("Enter USD: 1");
+        USD = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter VND: 23000");
+        VND = int.Parse(Console.ReadLine());
+        Console.WriteLine("amount of VND for 1 USD is: " + USD*VND);
+
+    }
+}
+}
